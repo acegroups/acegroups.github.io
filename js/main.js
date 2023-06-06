1addc4a44dfd001730d7dfda5841e07ada4427ee
ace studio <121967692+acegroups@users.noreply.github.com>
2023-06-06 14:22:37 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const container = document.querySelectorAll('.container');
+const body = document.body;
+const phoneImg = document.querySelector('.info__img img');   
+
+const bodyWidth = body.clientWidth;
+// const containerWidth = (bodyWidth / 1.6) + 30;
+
+// if (bodyWidth <= 1330) {
+
+//     container.forEach(item => {
+//         item.style.maxWidth = containerWidth + 'px';
+//     });
+// }
+// console.log(bodyWidth)
+// console.log(phoneImg)
+// if(bodyWidth <= 700) {
+//     phoneImg.src = 'img/mini_phone.png';
+// }
